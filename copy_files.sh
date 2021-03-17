5119504498d817c61d92f67f80401384a7d73c83
vsevolod <vsevolod026@gmail.com>
2021-03-17 18:47:18 +0300

    fixed i3 folder by changing copy_files.sh

--- copy_files.sh
+++ copy_files.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-cp -r ../i3 i3
+if [ ! -d "i3" ]; then
+    mkdir i3
+fi
+
+cp  ~/.config/i3/config ./i3/config
+cp  ~/.config/i3/picom.conf ./i3/picom.conf
 cp ~/.vimrc ./.vimrc
-cp -r ../i3status i3status
+cp -r ~/.config/i3status i3status
 sudo cp /usr/bin/start-script.sh ./start-script.sh
